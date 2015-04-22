f410d07e58b71e6f61d6ef8559e61ed85d3bec3c
Dmitry Gorlov <dimkahare@gmail.com>
2015-04-22 18:24:25 +0300

    Fixing. Accounts list fix

--- mkApi/mkWeb/app/components/accounts/accounts-controller.js
+++ mkApi/mkWeb/app/components/accounts/accounts-controller.js
@@ -1,22 +1,68 @@
 define(
     [
         'mkControllers',
+        'logger',
         './accounts-services'
     ],
-    function (mkControllers) {
+    function (mkControllers, logger) {
+        var updateAccountsList = function ($scope, accountsFactory) {
+            var result = new $.Deferred();
+
+            logger.time('Updating accounts list');
+
+            $scope.isUpdating = true;
+
+            accountsFactory.query(
+                function (accounts) {
+                    logger.timeEnd('Updating accounts list');
+                    logger.groupCollapsed('Updating accounts list');
+                    logger.logAccounts(accounts);
+                    logger.groupEnd('Updating accounts list');
+
+                    $scope.isUpdating = false;
+                    result.resolve(accounts);
+                },
+                function (error) {
+                    logger.error(error);
+                    $scope.isUpdating = false;
+                    result.reject(error);
+                }
+            );
+
+            return result.promise();
+        };
+
         mkControllers.controller(
             'AccountListCtrl',
             [
                 '$scope',
                 'Account',
                 function ($scope, Account) {
-                    $scope.accounts = Account.query();
+                    $scope.accounts = [];
+                    $scope.isUpdating = false;
                     $scope.orderProp = '_id';
+
+                    updateAccountsList($scope, Account)
+                        .done(function (accounts) {
+                            $scope.accounts = accounts
+                        })
+                        .fail(function (error) {
+                            $scope.accounts = [];
+                        });
+
+                    $scope.refresh = function () {
+                        updateAccountsList($scope, Account)
+                            .done(function (accounts) {
+                                $scope.accounts = accounts
+                            })
+                            .fail(function (error) {
+                                $scope.accounts = [];
+                            });
+                    };
 
                     $scope.showDetails = function (accountId) {
                         console.log('showDetails');
                         console.log(accountId);
-
                         window.location.hash = '#/accounts/' + accountId;
                     };
 
