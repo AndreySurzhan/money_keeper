5df66a136a6c9599b0d51523bb12c575d78780f0
Dmitry Gorlov <dimkahare@gmail.com>
2015-06-04 10:00:11 +0300

    f-redesign. Create categories tree #1

--- mkApi/mkWeb/app/components/categories/categories-controller.js
+++ mkApi/mkWeb/app/components/categories/categories-controller.js
@@ -6,7 +6,55 @@ define(
         './categories-services'
     ],
     function (mkControllers, logger, $) {
+        var sortableOptions = {
+            animation: 150,
+            handle: '.grad-handle'
+        };
+
+        var makeTree = function (currentNode, categories) {
+            console.log(categories);
+
+            var i;
+            var lastList;
+            var tree;
+
+            lastList = getCategoriesByParentId(categories, 'root');
+            tree = lastList;
+
+            for (i = 0; i < tree.length; i++) {
+
+            }
+
+            return tree;
+
+            function getCategoriesByParentId (categories, parentId) {
+                var i;
+                var index;
+                var result = [];
+
+                for (i = 0; i < categories.length; i++) {
+                    if (
+                        (_.isNull(categories[i].parent) && parentId === 'root') ||
+                        (!_.isNull(categories[i].parent) && categories[i].parent._id === parentId)
+                    ) {
+                        result.push(categories[i]);
+                    }
+                }
+
+                for (i = 0; i < result.length; i++) {
+                    index = _.indexOf(categories, result[i]);
+
+                    if (index > -1) {
+                        categories.splice(index, 1);
+                    }
+                }
+
+                return result;
+            }
+        };
+
         var updateCategoriesList = function ($scope, categoriesFactory) {
+            var categoriesTree;
             var result = new $.Deferred();
 
             logger.time('Updating categories list');
@@ -21,7 +69,10 @@ define(
                     logger.groupEnd('Updating categories list');
 
                     $scope.isUpdating = false;
-                    result.resolve(categories);
+
+                    categoriesTree = makeTree(categories);
+
+                    result.resolve(categoriesTree);
                 },
                 function (error) {
                     logger.error(error);
@@ -41,6 +92,7 @@ define(
                 function ($scope, Category) {
                     $scope.categories = [];
                     $scope.orderProp = '_id';
+                    $scope.sortableOptions = sortableOptions;
 
                     updateCategoriesList($scope, Category)
                         .done(function (categories) {
